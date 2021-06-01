3ed13562a83bef53ce8079fe089cd8ce44ba9f09
Msksgm <msksgm3124@gmail.com>
2021-06-02 07:55:37 +0900

    fix: prettierでformat

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import * as mysql from 'promise-mysql';
+import express from "express";
+import * as mysql from "promise-mysql";
 
 const app: express.Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(4000, () => {
-    console.log("Start on port 4000.")
+  console.log("Start on port 4000.");
 });
 
 const connection = async () => {
@@ -16,17 +16,19 @@ const connection = async () => {
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    multipleStatements: true
+    multipleStatements: true,
   });
 };
 
 // movie一覧取得
 app.get("/movie", (req: express.Request, res: express.Response) => {
-  connection().then(connection => {
-    const result = connection.query("SELECT * FROM MOVIE");
-    connection.end();
-    return result;
-  }).then(function(rows) {
-    res.send(rows);
-  });
+  connection()
+    .then((connection) => {
+      const result = connection.query("SELECT * FROM MOVIE");
+      connection.end();
+      return result;
+    })
+    .then(function (rows) {
+      res.send(rows);
+    });
 });
